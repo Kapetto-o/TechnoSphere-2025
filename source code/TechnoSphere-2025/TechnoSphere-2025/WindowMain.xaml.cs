96a7fa144c9ed620f10d71ae185fd8aa3137ba9a
Pavel <lenkevichpa@gmail.com>
2025-04-20 23:36:55 +0300

    Добавлены 2 страницы: регистрация, авторизаций; добавлены иконки

---
+++ source code/TechnoSphere-2025/TechnoSphere-2025/WindowMain.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TechnoSphere_2025;
+
+/// <summary>
+/// Interaction logic for WindowMain.xaml
+/// </summary>
+public partial class WindowMain : Window
+{
+    public WindowMain()
+    {
+        InitializeComponent();
+        MainFrame.Navigate(new AuthorizationPage());
+    }
+
+    private void Show_Click(object sender, RoutedEventArgs e)
+    {
+        this.WindowState = WindowState.Minimized;
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+    private void WindowControl_Moving_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+        {
+            this.DragMove();
+        }
+    }
+}
