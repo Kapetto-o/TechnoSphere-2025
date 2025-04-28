b71799633a2b02eb6339f773fb3b4d91074cdea8
Pavel <lenkevichpa@gmail.com>
2025-04-29 02:47:43 +0300

    Тема теперь выбирается при запуске и сохраняется для следующего запуска

--- source code/TechnoSphere-2025/TechnoSphere-2025/App.xaml.cs
+++ source code/TechnoSphere-2025/TechnoSphere-2025/App.xaml.cs
@@ -1,13 +1,14 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using TechnoSphere_2025.helper;
 
-namespace TechnoSphere_2025;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application
+namespace TechnoSphere_2025
 {
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ThemeManager.Initialize();
+        }
+    }
 }
-
