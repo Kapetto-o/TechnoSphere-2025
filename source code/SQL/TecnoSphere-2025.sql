8dbe85115a7b686349cba71d05cf43a7dc3ad062
Pavel <lenkevichpa@gmail.com>
2025-05-01 18:01:55 +0300

    Обновлена sql база пользователей. Добавлены тестовые пользователи

--- source code/SQL/TecnoSphere-2025.sql
+++ source code/SQL/TecnoSphere-2025.sql
@@ -7,17 +7,18 @@ use TechnoSphere_2025
 go
 
 create table Users (
-    UserID			int				identity(1,1)	primary key,
-    Username		nvarchar(32)	not null unique,
-    PasswordHash	varbinary(64)	not null,
-    PasswordSalt	varbinary(16)	not null,
-    Email			nvarchar(255)	not null unique,
-    FirstName		nvarchar(65)	null,
-    LastName		nvarchar(64)	null,
-    Phone			nvarchar(20)	null,
-    AddressLine		nvarchar(255)	null,
-	AvatarPath		nvarchar(255)	null,
+    UserID			int					identity(1,1)	primary key,
+    Username		nvarchar(32)		not null unique,
+    PasswordHash	varbinary(64)		not null,
+    PasswordSalt	varbinary(16)		not null,
+    Email			nvarchar(255)		not null unique,
+    FirstName		nvarchar(65)		null,
+    LastName		nvarchar(64)		null,
+    Phone			nvarchar(20)		null,
+    AddressLine		nvarchar(255)		null,
+	AvatarPath		nvarchar(255)		null,
     Role			tinyint			not null default(0),  -- 0 = покупатель, 1 = админ
-    IsActive		bit				not null default(1)   -- 1 = активен, 0 = заблокирован
+    IsActive		bit				not null default(1),  -- 1 = активен, 0 = заблокирован
+	RememberToken	uniqueidentifier	null
 );
 go
---
+++ source code/SQL/mainUsers.sql
@@ -0,0 +1,7 @@
+use TechnoSphere_2025
+go
+
+insert into Users (Username, PasswordHash, PasswordSalt, Email, AvatarPath, Role, IsActive)
+values ('admin', 0x7C69C71AADDDD7AC9FE7565E5682B65B495249303B9B8BE3C5FFB5B4F7BA04D1, 0xF1173165DA1CE4129E9FB764B5C03E5A, 'admin@gmail.com', null, 1, 1),
+	   ('user', 0x2C204F0C6E770A271E729F59354F27A29160BC68EA385DE51CB8C966E22BA566, 0x9435F34D5998896711C5AB15740C6C29, 'user@gmail.com', null, 0, 1)
+go
