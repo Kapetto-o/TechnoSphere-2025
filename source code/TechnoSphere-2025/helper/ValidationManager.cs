01e381e8f5f95562dbdac5ebb963a6e493f91288
Kapetto-o <lenkevichpa@gmail.com>
2025-05-01 14:27:10 +0300

    Валидация

---
+++ source code/TechnoSphere-2025/helper/ValidationManager.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechnoSphere_2025.helper
+{
+    internal class ValidationManager
+    {
+        public static List<ValidationResult> Validate(object obj)
+        {
+            var results = new List<ValidationResult>();
+            var ctx = new ValidationContext(obj);
+            Validator.TryValidateObject(obj, ctx, results, true);
+            return results;
+        }
+    }
+}
