55ab583e23f28b6c69caf6fdd0d66577885a0d3b
Kapetto-o <lenkevichpa@gmail.com>
2025-05-17 02:16:45 +0300

    Добавлены фото товаров и пути к ним в БД

--- source code/SQL/mainFilling.sql
+++ source code/SQL/mainFilling.sql
@@ -25,15 +25,15 @@ values
 
   -- Подкатегории для 'Холодильники' (ParentCategoryID = 1)
   ('Однокамерные',       'Single-chamber',      1,    1, 1),
-  ('Двухкамерные',       'Double-chamber',      1,    2, 1),
+  ('С морозильной камерой',       'Double-chamber',      1,    2, 1),
 
   -- Подкатегории для 'Стиральные машины' (ParentCategoryID = 2)
   ('Фронтальная загрузка','Front-loading',       2,    1, 1),
   ('Вертикальная загрузка','Top-loading',        2,    2, 1),
 
   -- Подкатегории для 'Пылесосы' (ParentCategoryID = 3)
-  ('Ручные пылесосы',    'Handheld vacuums',    3,    1, 1),
-  ('Роботы-пылесосы',    'Robot vacuums',       3,    2, 1);
+  ('С мешком для пыли',    'Handheld vacuums',    3,    1, 1),
+  ('С аквафильтром',    'Robot vacuums',       3,    2, 1);
 go
 
 insert into products
@@ -42,98 +42,98 @@ insert into products
      mainimagepath, mainimagealt_ru, mainimagealt_eng, categoryid)
 values
 -- подкатегория 4: однокамерные
-('frig-oc-001', 'Холодильник Однокамерный 1', 'Single-Chamber Fridge 1',
+('11111111', 'Liebherr SRbde 5220', 'Liebherr SRbde 5220',
  'Описание холодильника однокамерного 1', 'Description of single-chamber fridge 1',
- 25000.00, 2500.00, null, 10, 1,
- '/images/fridge_sc_1.jpg', 'Однокам. холодильник 1', 'Single-chamber fridge 1', 4),
+ 5310, 193, null, 10, 1,	
+ '/images/products/Liebherr_SRbde_5220.jpg', 'Однокам. холодильник 1', 'Single-chamber fridge 1', 4),
 
-('frig-oc-002', 'Холодильник Однокамерный 2', 'Single-Chamber Fridge 2',
+('11111112', 'Liebherr SRsfd 5220 Plus 5220-22001', 'Liebherr SRsfd 5220 Plus 5220-22001',
  'Описание холодильника однокамерного 2', 'Description of single-chamber fridge 2',
- 27000.00, 2700.00, 26000.00, 5, 1,
- '/images/fridge_sc_2.jpg', 'Однокам. холодильник 2', 'Single-chamber fridge 2', 4),
-
-('frig-oc-003', 'Холодильник Однокамерный 3', 'Single-Chamber Fridge 3',
+ 3851, 140, 1540, 5, 1,
+ '/images/products/Liebherr_SRsfd_5220_Plus_5220-22001.jpg', 'Однокам. холодильник 2', 'Single-chamber fridge 2', 4),
+	
+('11111113', 'Liebherr SRd 5220 Plus 5220-22001', 'Liebherr SRd 5220 Plus 5220-22001',
  'Описание холодильника однокамерного 3', 'Description of single-chamber fridge 3',
- 23000.00, null, null, 0, 1,
- '/images/fridge_sc_3.jpg', 'Однокам. холодильник 3', 'Single-chamber fridge 3', 4),
+ 2938, null, null, 0, 1,
+ '/images/products/Liebherr_SRd_5220_Plus_5220-22001.jpg', 'Однокам. холодильник 3', 'Single-chamber fridge 3', 4),
 
 -- подкатегория 5: двухкамерные
-('frig-dc-001', 'Холодильник Двухкамерный 1', 'Double-Chamber Fridge 1',
+('11111114', ' Liebherr CNsff 5703 Pure NoFrost 5703', 'Liebherr CNsff 5703 Pure NoFrost 5703',
  'Описание холодильника двухкамерного 1', 'Description of double-chamber fridge 1',
- 35000.00, 3500.00, 33000.00, 8, 1,
- '/images/fridge_dc_1.jpg', 'Двухкам. холодильник 1', 'Double-chamber fridge 1', 5),
+ 3199, 116, 1215, 8, 1,
+ '/images/products/Liebherr_CNsff_5703_Pure_NoFrost_5703.jpg', 'Двухкам. холодильник 1', 'Double-chamber fridge 1', 5),
 
-('frig-dc-002', 'Холодильник Двухкамерный 2', 'Double-Chamber Fridge 2',
+('11111115', 'Liebherr CBNbdc 573i Plus BioFresh NoFrost 573i', 'Liebherr CBNbdc 573i Plus BioFresh NoFrost 573i',
  'Описание холодильника двухкамерного 2', 'Description of double-chamber fridge 2',
- 37000.00, null, null, 12, 1,
- '/images/fridge_dc_2.jpg', 'Двухкам. холодильник 2', 'Double-chamber fridge 2', 5),
+ 5999, null, null, 12, 1,
+ '/images/products/Liebherr_CBNbdc_573i_Plus_BioFresh_NoFrost_573i.jpg', 'Двухкам. холодильник 2', 'Double-chamber fridge 2', 5),
 
-('frig-dc-003', 'Холодильник Двухкамерный 3', 'Double-Chamber Fridge 3',
+('11111116', 'Liebherr CNgwc 5723 Plus NoFrost 5723', 'Liebherr CNgwc 5723 Plus NoFrost 5723',
  'Описание холодильника двухкамерного 3', 'Description of double-chamber fridge 3',
- 39000.00, 3900.00, null, 3, 1,
- '/images/fridge_dc_3.jpg', 'Двухкам. холодильник 3', 'Double-chamber fridge 3', 5),
+ 4856, 176, null, 3, 1,
+ '/images/products/Liebherr_CNgwc_5723_Plus_NoFrost_5723.jpg', 'Двухкам. холодильник 3', 'Double-chamber fridge 3', 5),
 
 -- подкатегория 6: фронтальная загрузка
-('wm-fl-001', 'Стиральная машина Фронт. 1', 'Front-Load Washer 1',
+('11111117', 'Bosch WGG0420GPL', 'Bosch WGG0420GPL',
  'Описание фронтальной стиралки 1', 'Description of front-load washer 1',
- 20000.00, 2000.00, null, 7, 1,
- '/images/washer_fl_1.jpg', 'Фронт. стиралка 1', 'Front-load washer 1', 6),
+ 2420, 88, null, 7, 1,
+ '/images/products/Bosch_WGG0420GPL.jpg', 'Фронт. стиралка 1', 'Front-load washer 1', 6),
 
-('wm-fl-002', 'Стиральная машина Фронт. 2', 'Front-Load Washer 2',
+('11111118', 'Electrolux EW8FN148BP', 'Electrolux EW8FN148BP',
  'Описание фронтальной стиралки 2', 'Description of front-load washer 2',
- 22000.00, null, 21000.00, 4, 1,
- '/images/washer_fl_2.jpg', 'Фронт. стиралка 2', 'Front-load washer 2', 6),
+ 3020, null, 2710, 4, 1,
+ '/images/products/Electrolux_EW8FN148BP.jpg', 'Фронт. стиралка 2', 'Front-load washer 2', 6),
 
-('wm-fl-003', 'Стиральная машина Фронт. 3', 'Front-Load Washer 3',
+('11111119', 'Bosch WGG242ZGPL', 'Bosch WGG242ZGPL',
  'Описание фронтальной стиралки 3', 'Description of front-load washer 3',
- 24000.00, 2400.00, null, 0, 1,
- '/images/washer_fl_3.jpg', 'Фронт. стиралка 3', 'Front-load washer 3', 6),
+ 2780, 101, null, 0, 1,
+ '/images/products/Bosch_WGG242ZGPL.jpg', 'Фронт. стиралка 3', 'Front-load washer 3', 6),
 
 -- подкатегория 7: вертикальная загрузка
-('wm-tl-001', 'Стиральная машина Верхн. 1', 'Top-Load Washer 1',
+('11111121', 'Whirlpool TDLR 6240L EU/N', 'Whirlpool TDLR 6240L EU/N',
  'Описание вертикальной стиралки 1', 'Description of top-load washer 1',
- 18000.00, null, null, 6, 1,
- '/images/washer_tl_1.jpg', 'Верхн. стиралка 1', 'Top-load washer 1', 7),
+ 1406, null, null, 6, 1,
+ '/images/products/Whirlpool_TDLR_6240L_EU-N.jpg', 'Верхн. стиралка 1', 'Top-load washer 1', 7),
 
-('wm-tl-002', 'Стиральная машина Верхн. 2', 'Top-Load Washer 2',
+('11111122', 'Whirlpool TDLR 7231BS EU', 'Whirlpool TDLR 7231BS EU',
  'Описание вертикальной стиралки 2', 'Description of top-load washer 2',
- 19000.00, 1900.00, 18000.00, 2, 1,
- '/images/washer_tl_2.jpg', 'Верхн. стиралка 2', 'Top-load washer 2', 7),
+ 1750, 64, 1400, 2, 1,
+ '/images/products/Whirlpool_TDLR_7231BS_EU.jpg', 'Верхн. стиралка 2', 'Top-load washer 2', 7),
 
-('wm-tl-003', 'Стиральная машина Верхн. 3', 'Top-Load Washer 3',
+('11111123', 'Weissgauff WM 40275 TD', 'Weissgauff WM 40275 TD',
  'Описание вертикальной стиралки 3', 'Description of top-load washer 3',
  21000.00, null, null, 9, 1,
- '/images/washer_tl_3.jpg', 'Верхн. стиралка 3', 'Top-load washer 3', 7),
+ '/images/products/Weissgauff_WM_40275_TD.jpg', 'Верхн. стиралка 3', 'Top-load washer 3', 7),
 
--- подкатегория 8: ручные пылесосы
-('vac-hh-001', 'Ручной пылесос 1', 'Handheld Vacuum 1',
+-- подкатегория 8: с мешком для пыли
+('11111124', 'Bosch BGLS4POW2', 'Bosch BGLS4POW2',
  'Описание ручного пылесоса 1', 'Description of handheld vacuum 1',
- 5000.00, 500.00, null, 15, 1,
- '/images/vac_hh_1.jpg', 'Ручн. пылесос 1', 'Handheld vacuum 1', 8),
+ 695, 26, null, 15, 1,
+ '/images/products/Bosch_BGLS4POW2.jpg', 'Ручн. пылесос 1', 'Handheld vacuum 1', 8),
 
-('vac-hh-002', 'Ручной пылесос 2', 'Handheld Vacuum 2',
+('11111125', 'Bosch BGL38BA2H', 'Bosch BGL38BA2H',
  'Описание ручного пылесоса 2', 'Description of handheld vacuum 2',
- 5500.00, null, 5300.00, 11, 1,
- '/images/vac_hh_2.jpg', 'Ручн. пылесос 2', 'Handheld vacuum 2', 8),
+ 593, null, 474, 11, 1,
+ '/images/products/Bosch_BGL38BA2H.jpg', 'Ручн. пылесос 2', 'Handheld vacuum 2', 8),
 
-('vac-hh-003', 'Ручной пылесос 3', 'Handheld Vacuum 3',
+('11111126', 'Bosch BGLS4PET2', 'Bosch BGLS4PET2',
  'Описание ручного пылесоса 3', 'Description of handheld vacuum 3',
- 6000.00, 600.00, null, 0, 1,
- '/images/vac_hh_3.jpg', 'Ручн. пылесос 3', 'Handheld vacuum 3', 8),
+ 617, 23, null, 0, 1,
+ '/images/products/Bosch_BGLS4PET2.jpg', 'Ручн. пылесос 3', 'Handheld vacuum 3', 8),
 
--- подкатегория 9: роботы-пылесосы
-('vac-rb-001', 'Робот-пылесос 1', 'Robot Vacuum 1',
+-- подкатегория 9: с аквафильтром
+('11111127', 'Bosch BWD421PRO', 'Bosch BWD421PRO',
  'Описание робота-пылесоса 1', 'Description of robot vacuum 1',
- 20000.00, 2000.00, 18000.00, 5, 1,
- '/images/vac_rb_1.jpg', 'Робот-пылесос 1', 'Robot vacuum 1', 9),
+ 1025, 38, 512, 5, 1,
+ '/images/products/Bosch_BWD421PRO.jpg', 'Робот-пылесос 1', 'Robot vacuum 1', 9),
 
-('vac-rb-002', 'Робот-пылесос 2', 'Robot Vacuum 2',
+('11111128', 'Bosch BWD41740', 'Bosch BWD41740',
  'Описание робота-пылесоса 2', 'Description of robot vacuum 2',
- 22000.00, null, null, 3, 1,
- '/images/vac_rb_2.jpg', 'Робот-пылесос 2', 'Robot vacuum 2', 9),
+ 1193, null, null, 3, 1,
+ '/images/products/Bosch_BWD41740.jpg', 'Робот-пылесос 2', 'Robot vacuum 2', 9),
 
-('vac-rb-003', 'Робот-пылесос 3', 'Robot Vacuum 3',
+('11111129', 'Bosch BWD421POW', 'Bosch BWD421POW	',
  'Описание робота-пылесоса 3', 'Description of robot vacuum 3',
- 25000.00, 2500.00, null, 0, 1,
- '/images/vac_rb_3.jpg', 'Робот-пылесос 3', 'Robot vacuum 3', 9);
+ 1051, 39, null, 0, 1,
+ '/images/products/Bosch_BWD421POW.jpg', 'Робот-пылесос 3', 'Robot vacuum 3', 9);
 go
