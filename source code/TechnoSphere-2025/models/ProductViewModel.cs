c57dcd12df5f260d076f2546972e6d559e863f4c
Kapetto-o <lenkevichpa@gmail.com>
2025-05-17 03:04:02 +0300

    Очистка кода, добавление дизайна, незначительные изменения

--- source code/TechnoSphere-2025/models/ProductViewModel.cs
+++ source code/TechnoSphere-2025/models/ProductViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using TechnoSphere_2025.managers;
 
 namespace TechnoSphere_2025.models
