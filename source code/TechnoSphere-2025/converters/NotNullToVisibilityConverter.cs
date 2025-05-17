cf0cd0a99dd30ca6e8f7efc1dae23945368092db
Pavel <lenkevichpa@gmail.com>
2025-05-17 20:31:47 +0300

    Добавлены фотографии к товарам

---
+++ source code/TechnoSphere-2025/converters/NotNullToVisibilityConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TechnoSphere_2025.converters
+{
+    public class NotNullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value != null
+               ? Visibility.Visible
+               : Visibility.Collapsed;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
