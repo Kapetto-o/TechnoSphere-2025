9aa583fe97a254c84be3a4e94003cbddf4518cd6
Pavel <lenkevichpa@gmail.com>
2025-05-17 21:58:27 +0300

    Акции теперь отображаются в карточке товара

--- source code/TechnoSphere-2025/converters/NullToVisibilityInverseConverter.cs
+++ source code/TechnoSphere-2025/converters/NullToVisibilityInverseConverter.cs
@@ -9,12 +9,10 @@ using System.Windows;
 
 namespace TechnoSphere_2025.converters
 {
-    public class NotNullToVisibilityConverter : IValueConverter
+    public class NullToVisibilityInverseConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => value != null
-               ? Visibility.Visible
-               : Visibility.Collapsed;
+            => value == null ? Visibility.Visible : Visibility.Collapsed;
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
             => throw new NotSupportedException();
